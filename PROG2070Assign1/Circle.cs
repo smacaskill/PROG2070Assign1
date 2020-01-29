bcb258b2c20817ff3a91e3c365f24f76d37a7e25
sandy.macaskill <joekool60@gmail.com>
2020-01-29 11:28:01 -0500

    Second iteration, contains Circle Class

---
+++ PROG2070Assign1/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070Assign1
+{
+    public class Circle
+    {
+        private int radius;
+
+        public Circle()
+        {
+            radius = 1;
+        }
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetCircumference()
+        {
+
+            return 2*Math.PI*radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
